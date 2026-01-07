01398fa542342aa8ffa94460433afca47a6979eb
dohuy8904 <doquanghuy8904@gmail.com>
2026-01-07 11:46:03 +0700

    update

--- chatbotdienthoai/chatbotdienthoai/src/main/java/com/vti/chatbotdienthoai/ChatbotdienthoaiApplication.java
+++ chatbotdienthoai/chatbotdienthoai/src/main/java/com/vti/chatbotdienthoai/ChatbotdienthoaiApplication.java
@@ -9,5 +9,4 @@ public class ChatbotdienthoaiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatbotdienthoaiApplication.class, args);
 	}
-
 }
---
+++ chatbotdienthoai/chatbotdienthoai/src/main/java/com/vti/chatbotdienthoai/repository/MongoRepository.java
@@ -0,0 +1,4 @@
+package com.vti.chatbotdienthoai.repository;
+
+public interface MongoRepository<T, T1> {
+}
